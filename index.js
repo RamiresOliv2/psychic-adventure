acaa771a9f630cc43cd78b7b982cf1fb15702979
Gabriel <ramires.oliveira@escola.pr.gov.br>
2024-11-27 12:42:01 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("sfdpasdifjmsadopkifasdofksdnf")
+const a = [
+    "https://www.youtube.com/embed/YjQVS3CmtVg?si=lFy5LxbdyNhbUP8p"
+]
+
+window.onload = () => {
+    const videosDiv = document.getElementById("videos");
+    const catto = document.getElementById("catto");
+
+
+    const headers = new Headers({
+        "Content-Type": "application/json",
+        "x-api-key": "DEMO-API-KEY"
+      });
+      
+      var requestOptions = {
+        method: 'GET',
+        headers: headers,
+        redirect: 'follow'
+      };
+
+    fetch("https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1", requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            console.log(result[0].url)
+            catto.src = result[0].url
+        })
+        .catch(error => console.log('error', error));        
+
+    for (const i in a) {
+        const url = a[i];
+        videosDiv.innerHTML = videosDiv.innerHTML + `<iframe width="560" height="315" src="${url}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+    }
+}
